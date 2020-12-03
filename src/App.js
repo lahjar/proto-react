754a2ec50b83f24bfa3a7d8964a5a6bc5a572a0d
ankit <kr.ankit00@gmail.com>
2020-12-03 22:33:19 +0530

    Card Layout

--- src/App.js
+++ src/App.js
@@ -1,119 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// class Square extends React.Component {
-//     render() {
-//       return (
-//         <button
-//           className="square"
-//           onClick={() => this.props.onClick()}
-//         >
-//           {this.props.value}
-//         </button>
-//       );
-//     }
-//   }
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import App from './App';
 
-  function Square(props) {
-      return (
-          <button className="square" onClick={props.onClick}>
-            {props.value}
-          </button>
-      );
-  }
 
-  class Board extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        squares: Array(9).fill(null),
-        xIsNext: true,
-      };
-    }
-  
-    handleClick(i) {
-      const squares = this.state.squares.slice();
-      if (calculateWinner(squares) || squares[i]) {      
-          return;    
-    }
-      squares[i] = (this.state.xIsNext ? 'X' : 'O');
-      this.setState({squares: squares, xIsNext: !this.state.xIsNext});
-    }
-  
-    renderSquare(i) {
-      return (
-        <Square
-          value={this.state.squares[i]}
-          onClick={() => this.handleClick(i)}
-        />
-      );
-    }
-  
-    render() {
-      const winner = calculateWinner(this.state.squares)
-      let status;
-      if (winner) {
-          status = 'Winner: ' + winner;
-      }
-      else {
-          status = 'Next Player: ' + (this.state.xIsNext ? 'X': 'O');
-      }        
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
-          </div>
-        </div>
-      );
-    }
-  }
-  
-  class Game extends React.Component {
-    render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-      );
-    }
-  }
-  
-  function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
 
-  // ========================================
-  
-  ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
+export default function SimpleCard() {
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
+
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="h2">
+          be{bull}nev{bull}o{bull}lent
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          adjective
+        </Typography>
+        <Typography variant="body2" component="p">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
   );
+}
+  
+
+ReactDOM.render(<App />, document.querySelector('#root'));
   
